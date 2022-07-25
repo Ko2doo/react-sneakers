3f9b23326fe3a6098acec0a9dcf5423c3e163e27
Ko2doo <ko2doodev@gmail>
2022-07-25 15:39:44 +0500

    Требуется доработка состояния содержимого Избранных товаров, возможно сделано колхозно, но оно работает -__-. Так же нужно поработать с ключами элементов key, для компонентов, есть проблемы с ключами дубликатами при удалении айтимсов с подобными, удаляются оба элемента если даже их содержимое разное

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -17,16 +17,37 @@ function Favorites(props) {
     />
   ));
 
+  let items = props.itemsFavorite;
+
   return (
     <section className={style.content}>
-      <div className={style.header_bar}>
-        <Link to="/" className={style.btn_goback} title="На главную">
-          <img className={style.btn_icon} src="/img/icons/ic-right.svg" alt="icon item" />
-        </Link>
-        <h1 className={style.content_title}>Мои закладки</h1>
-      </div>
-
-      {favoriteCard}
+      {items.length > 0 ? (
+        <div className={style.content_wrapper}>
+          <div className={style.header_bar}>
+            <Link to="/" className={style.btn_goback} title="На главную">
+              <img className={style.btn_icon} src="/img/icons/ic-right.svg" alt="icon item" />
+            </Link>
+            <h1 className={style.content_title}>Мои закладки</h1>
+          </div>
+          {favoriteCard}
+        </div>
+      ) : (
+        <div className={style.content_wrapper}>
+          <div className={style.empty_section}>
+            <img src="/img/icons/emoji.svg" alt="плак плак" />
+            <h1 className={style.empty_title}>Ничего нет</h1>
+            <p className={style.empty_subtitle}>Вы ничего не добавили в избранное</p>
+            <Link to="/" className={style.link_goback} title="Вернуться назад">
+              <span className={style.btn_name}>Вернуться назад</span>
+              <img
+                className={style.ic_arrow_left}
+                src="/img/icons/ic-arrow-left.svg"
+                alt="Icon item"
+              />
+            </Link>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
